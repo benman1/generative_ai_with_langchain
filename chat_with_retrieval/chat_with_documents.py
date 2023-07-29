4009dbd4fe52e132125cc545479d9a495f18dee0
Ben Auffarth <auffarth@gmail.com>
2023-07-29 16:33:19 +0100

    chatbot

---
+++ chat_with_retrieval/chat_with_documents.py
@@ -0,0 +1,73 @@
+"""Chat with retrieval and embeddings.
+
+Run like this:
+> PYTHONPATH=. streamlit run chat_with_retrieval/chat_with_documents.py
+"""
+import os
+import tempfile
+import streamlit as st
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chains import OpenAIModerationChain, SimpleSequentialChain
+
+from chat_with_retrieval.utils import (
+    load_document, DocumentLoader,
+    configure_retriever,
+    configure_chain, PrintRetrievalHandler
+)
+
+st.set_page_config(page_title="LangChain: Chat with Documents", page_icon="🦜")
+st.title("🦜 LangChain: Chat with Documents")
+
+
+def configure_retrieval_chain(
+        uploaded_files,
+        use_compression: bool = False,
+        use_flare: bool = False,
+        use_moderation: bool = False
+):
+    """Read documents, configure retriever, and the chain."""
+    docs = []
+    temp_dir = tempfile.TemporaryDirectory()
+    for file in uploaded_files:
+        temp_filepath = os.path.join(temp_dir.name, file.name)
+        with open(temp_filepath, "wb") as f:
+            f.write(file.getvalue())
+        docs.extend(load_document(temp_filepath))
+
+    retriever = configure_retriever(docs=docs, use_compression=use_compression)
+    chain = configure_chain(retriever=retriever, use_flare=use_flare)
+    if not use_moderation:
+        return chain
+
+    moderation_chain = OpenAIModerationChain()
+    return SimpleSequentialChain(chains=[chain, moderation_chain])
+
+
+uploaded_files = st.sidebar.file_uploader(
+    label="Upload files",
+    type=list(DocumentLoader.supported_extentions.keys()),
+    accept_multiple_files=True
+)
+if not uploaded_files:
+    st.info("Please upload documents to continue.")
+    st.stop()
+
+# use compression by default:
+use_compression = st.checkbox("compression", value=True)
+use_flare = st.checkbox("flare", value=True)
+use_moderation = st.checkbox("moderation", value=False)
+
+user_query = st.chat_input(placeholder="Ask me anything!")
+assistant = st.chat_message("assistant")
+
+CONV_CHAIN = configure_retrieval_chain(
+    uploaded_files,
+    use_compression=use_compression,
+    use_flare=use_flare,
+    use_moderation=use_moderation
+)
+
+if user_query:
+    stream_handler = StreamlitCallbackHandler(assistant)
+    response = CONV_CHAIN.run(user_query, callbacks=[stream_handler])
+    st.markdown(response)
---
+++ chat_with_retrieval/utils.py
@@ -0,0 +1,143 @@
+"""Document loading functionality."""
+import logging
+import os
+import pathlib
+from typing import Any
+import streamlit as st
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationalRetrievalChain, FlareChain
+from langchain.chains.base import Chain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import (
+    PyPDFLoader, UnstructuredEPubLoader,
+    UnstructuredWordDocumentLoader, TextLoader
+)
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain.schema import Document, BaseRetriever
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+
+from config import set_environment
+
+LOGGER = logging.getLogger()
+set_environment()
+# Setup LLM and QA chain; set temperature low to keep hallucinations in check
+LLM = ChatOpenAI(
+    model_name="gpt-3.5-turbo", temperature=0, streaming=True
+)
+
+
+@st.cache_resource
+def init_memory():
+    """Initialize the memory for contextual conversation.
+
+    We are caching this, so it won't be deleted
+     every time, we restart the server.
+     """
+    return ConversationBufferMemory(
+        llm=LLM,
+        return_messages=True
+    )
+
+class EpubReader(UnstructuredEPubLoader):
+    def __init__(self, file_path: str | list[str], **unstructured_kwargs: Any):
+        super().__init__(file_path, **unstructured_kwargs, mode="elements", strategy="fast")
+
+
+class DocumentLoaderException(Exception):
+    pass
+
+
+class DocumentLoader(object):
+    """Loads in a document with a supported extension."""
+    supported_extentions = {
+        ".pdf": PyPDFLoader,
+        ".txt": TextLoader,
+        ".epub": EpubReader,
+        ".docx": UnstructuredWordDocumentLoader,
+        ".doc": UnstructuredWordDocumentLoader
+    }
+
+
+def load_document(temp_filepath: str) -> list[Document]:
+    """Load a file and return it as a list of documents.
+
+    Doesn't handle a lot of errors at the moment.
+    """
+    ext = pathlib.Path(temp_filepath).suffix
+    loader = DocumentLoader.supported_extentions.get(ext)
+    if not loader:
+        raise DocumentLoaderException(
+            f"Invalid extension type {ext}, cannot load this type of file"
+        )
+
+    loader = loader(temp_filepath)
+    docs = loader.load()
+    logging.info(docs)
+    return docs
+
+
+def configure_retriever(
+        docs: list[Document],
+        use_compression: bool = False
+) -> BaseRetriever:
+    """Retriever to use."""
+    # Split each document documents:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+
+    # Create embeddings and store in vectordb:
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    # Create vectordb with single call to embedding model for texts:
+    vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
+    retriever = vectordb.as_retriever(
+        search_type="mmr", search_kwargs={
+            "k": 2,
+            "fetch_k": 4,
+            "include_metadata": True
+        },
+    )
+    if not use_compression:
+        return retriever
+
+    embeddings_filter = EmbeddingsFilter(
+        embeddings=embeddings, similarity_threshold=0.2
+    )
+    return ContextualCompressionRetriever(
+        base_compressor=embeddings_filter,
+        base_retriever=retriever,
+    )
+
+
+def configure_chain(retriever: BaseRetriever, use_flare: bool = True) -> Chain:
+    """Configure chain with a retriever.
+
+    Passing in a max_tokens_limit amount automatically
+    truncates the tokens when prompting your llm!
+    """
+    chain = FlareChain if use_flare else ConversationalRetrievalChain
+    return chain.from_llm(
+        LLM,
+        retriever=retriever,
+        memory=init_memory(),
+        verbose=True,
+        max_tokens_limit=4000,
+    )
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.container = container.expander("Context Retrieval")
+
+    def on_retriever_start(self, query: str, **kwargs):
+        self.container.write(f"**Question:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.container.write(f"**Document {idx} from {source}**")
+            self.container.markdown(doc.page_content)
