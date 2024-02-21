4613073fa5750eb5396a3e85e9cd49f6141156bc
Ben Auffarth <10786684+benman1@users.noreply.github.com>
2024-02-21 15:41:53 +0000

    remove extra specification

--- summarize/prompts.py
+++ summarize/prompts.py
@@ -9,8 +9,6 @@ Article: {text}
 
 You will generate increasingly concise entity-dense summaries of the generated summary. 
 
-For the different areas such as data collection, visualization, and exploratory data analysis (EDA), pre-processing and feature engineering, and AutoML, I want to highlight the main points starting from their importance and impact, mechanics, available tools, and potential extensions that impact dimensions such as privacy, safety flexibility, competitive performance, ease of use.
-
 Repeat the following 2 steps 5 times:
 Step 1: Identify 1-3 informative entities (";" delimited) from the previous generated version of the summary. 
 Step 2: Write a new, denser summary of shorter length that covers every entity mentioned, plus missing entities.
